053466867d7b7683bcfb5fa40ccccdb2eae997d9
Sydnee Sampson <sbsydnee161416@gmail.com>
2019-09-21 07:34:05 +0000

    Done.

---
+++ lib/patient.rb
@@ -0,0 +1,21 @@
+class Patient
+  attr_accessor :name, :appointment, :patient, :doctor 
+  @@all = []
+  def initialize(name)
+    @name = name 
+    @@all << self
+  end
+  def self.all 
+    @@all
+  end
+  def appointments
+    Appointment.all.select {|appointment| appointment.patient == self}
+  end
+  def doctors 
+    appointments.collect {|appointment| appointment.doctor}
+    end
+  def new_appointment(doctor, date) 
+    Appointment.new(date, self, doctor)
+  end 
+end 
+
